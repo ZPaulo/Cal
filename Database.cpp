199b5e4f448b81357ce48151cfa3330c3dba88ab
ZPaulo <jose.p.s.olive@gmail.com>
2014-06-02 11:02:34 +0100

    new
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- Database.cpp
+++ Database.cpp
@@ -43,51 +43,61 @@ void Database::initialize()
 }
 void Database:: matches()
 {
-	int count, totalWords;//contagem de palavras
+	int countInt,countAre, totalWords;//contagem de palavras
 	bool hasMatches = false;//verfica se houve correspondências entre um user e alguma das conferências
-	string interestsName, areas;
+	string interestsName, areas, conferenceName;
 
 	for(int i = 0; i < users.size(); i++)//percorre todos os utilizadores
 	{
 		cout << "The User: " << users[i].getName() << endl;
-		count = 0;
-		interestsName = "";
-		areas ="";
 		vector<string> area = users[i].getArea();
 		vector<string> interests = users[i].getInterests();
-		totalWords = area.size() + interests.size();//actualiza o total de palavras que estão a ser procuradas
+		totalWords = interests.size();//actualiza o total de palavras que estão a ser procuradas
+		conferenceName = "";
 
 		for(int j = 0; j < conferences.size();j++)//percorre as conferências todas
 		{
-			for(int k = 0; k < area.size();k++)//precorre todas as áreas do utilizador
+			interestsName = "";
+			areas = "";
+			countAre = 0;
+			countInt = 0;
+			for(int k = 0; k < area.size();k++)//percorre todas as áreas do utilizador
 			{
 				if(KMP(conferences[j].getText(),area[k]))//procura a área na conferência
 				{
-					count++;
+					countAre++;
 					if (!KMP (areas, area[k]))
 						areas += area[k]  + " ";
 				}
 			}
-			if(count > 0)//se nao encontrou nenhuma área, não procura os interesses
+			if(countAre > 0)//se nao encontrou nenhuma área, não procura os interesses
 			{
 				for(int k = 0; k < interests.size();k++)
 				{
 					if(KMP(conferences[j].getText(),interests[k])){
-						count++;
+						countInt++;
 						if (!KMP (interestsName, interests[k]))
 							interestsName += interests[k]  + " ";
 					}
 				}				
 			}
-			if ((double) count /totalWords > 0)//caso tenha encontrado pelo menos uma correspondência nesta conferência
+			if ((double) countInt /totalWords > 0)//caso tenha encontrado pelo menos uma correspondência nesta conferência
 			{
-				cout <<  "- Has " << (double) count/totalWords <<  "% matches with the conference: " <<  conferences[j].getName() << "\n\tIn the areas: " << areas <<"\n\tWith the interests: "  << interestsName << endl;
+				double match = (double)80 * (log(countInt) / log(totalWords)) + countAre*(20 / area.size());
+				cout <<  "- Has " << match <<  "% matches with the conference: " <<  conferences[j].getName() << "\n\tIn the areas: " << areas <<"\n\tWith the interests: "  << interestsName << endl;
 				hasMatches = true;
+				if (match > 70)
+					conferenceName += conferences[j].getName() + " ";
+				
 			}
 
 		}
-		if(!hasMatches)//caso não tenha encontrado nenhuma correspondência em nenhuma conferência
+
+		if (!hasMatches)//caso não tenha encontrado nenhuma correspondência em nenhuma conferência
 			cout << "No matches were found!" << endl;
+		else if (conferenceName != "")
+			cout << endl << endl << users[i].getName() << " has the following suggested conferences: " << conferenceName << endl;
+			
 		hasMatches = false;
 		cout << endl;
 	}
